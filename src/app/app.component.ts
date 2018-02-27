ce2958cde9e9fa5eedb74ebe984a24116147e32c
Sam Gespass <darth.toad@gmail.com>
2018-02-27 14:40:53 -0800

    fix more bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent implements OnInit {
     this.observableWord = this.appService.getWord();
     this.observableWord.subscribe(
       word => this.currentWord = word,
-      error => this.errorMessage = <any>error;
+      error => this.errorMessage = <any>error
     );
   }
 
@@ -27,7 +27,7 @@ export class AppComponent implements OnInit {
     this.observableWord = this.appService.getWord();
     this.observableWord.subscribe(
       word => this.currentWord = word,
-      error => this.errorMessage = <any>error;
+      error => this.errorMessage = <any>error
     );
   }
 }
